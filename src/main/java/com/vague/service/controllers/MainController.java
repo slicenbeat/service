99de9b906bd0d6a0bf3b4a131b3c74c0aae94d62
Danil <Danil@DESKTOP-OPOG935>
2021-05-14 13:56:20 +0400

    версия 0.3.0

--- src/main/java/com/vague/service/controllers/MainController.java
+++ src/main/java/com/vague/service/controllers/MainController.java
@@ -1,5 +1,6 @@
 package com.vague.service.controllers;
 import com.vague.service.models.Users;
+import com.vague.service.repo.BlackListRepo;
 import com.vague.service.repo.UsersRepo;
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
@@ -7,26 +8,74 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequestMapping("/vague")
+@Controller
+//@RequestMapping
 @AllArgsConstructor
 @Log
 @CrossOrigin
 public class MainController {
 
-    private final UsersRepo usersRepo;
-
-    @GetMapping("/login")
-    public String index(){
-        return "index";
+    @GetMapping(value = "/usermain")
+    public String userMainView(){
+        return "usermain";
     }
 
-    @PostMapping()
-    public /*@ResponseBody*/ Users newUser(@RequestBody Users user){
-        return usersRepo.save(user);
-        //return "match";
+    @GetMapping(value = "/match")
+    public String getMatch(){
+        return "match";
     }
+
+    @GetMapping(value = "/questionnaires")
+    public String getQuestionnaires(){
+        return "questionnaires";
+    }
+
+    @GetMapping(value = "/settings")
+    public String getSettings(){
+        return "settings";
+    }
+
+    @GetMapping(value = "/adminmain")
+    public String adminMainView(){
+        return "adminmain";
+    }
+
+    @GetMapping(value = "/statistics")
+    public String statisticsView(){
+        return "statistics";
+    }
+
+    @GetMapping(value = "/blacklist")
+    public String blackListView(){
+        return "blacklist";
+    }
+    /*@GetMapping(value = "/login")
+    public String getPage(@RequestBody Users user){
+        System.out.println("sdfgvdfdfdvf");
+        return "/blacklist";
+    }*/
+
+    /*@DeleteMapping(value = "/login")
+    public void deleteUserFromBlackList(@RequestBody str login){
+        System.out.println(login.getLogin());
+        blackListRepo.deleteById(login.getLogin());
+    }*/
+
+
+    /*@GetMapping("/login")
+    public ModelAndView NewView(){
+        ModelAndView mav = new ModelAndView("blacklist");
+        System.out.println("rjrj");
+        return mav;
+    }*/
+
+    //@PostMapping()
+    //public String newUser(@RequestBody Users user){
+    //    return usersRepo.save(user);
+    //    //return "match";
+    //}
 
     //@GetMapping("/match")
     //public String Match()
