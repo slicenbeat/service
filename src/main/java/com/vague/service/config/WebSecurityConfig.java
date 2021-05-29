27f303d7fd3527f0359f83a3954f30e0e5f99f67
slicenbeat <slicenbeat@gmail.com>
2021-05-29 18:59:11 +0400

    Подключил дисигн для входа, регистрации и главной страницы

--- src/main/java/com/vague/service/config/WebSecurityConfig.java
+++ src/main/java/com/vague/service/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -15,6 +16,13 @@ import javax.sql.DataSource;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/styles/**", "/js/**", "/pics/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
